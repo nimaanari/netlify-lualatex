c894febb7e561c5cc51b69a1d9f7fd97fcdde419
Nima Anari <anari.nima@gmail.com>
2019-09-16 23:58:02 -0700

    Working version

--- src/index.js
+++ src/index.js
@@ -4,41 +4,96 @@ import tmp from 'tmp-promise';
 import stream from 'stream';
 import path from 'path';
 import { promisify } from 'util';
-import { spawn } from 'child-process-promise';
+import { spawn, exec } from 'child-process-promise';
 import fs from 'fs-extra';
+import lockfile from 'proper-lockfile';
+import os from 'os';
+import whichOriginal from 'which';
 
 import profile from './profile';
 
 const pipeline = promisify(stream.pipeline);
+const which = promisify(whichOriginal);
 
 const tarURL = 'http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz';
-const installerName = 'install-tl';
+const searchForLocalTexlive = true;
 const arch = `${{x86: 'x86_32', x64: 'x86_64'}[process.arch]}-${process.platform}`;
 
-export const installTexlive = async (basePath) => {
-    const { path: tmpPath, cleanup } = await tmp.dir({ unsafeCleanup: true });
+const installTexlive = async (basePath) => {
+    const checkPath = path.join(basePath, 'latest');
+    const release = await lockfile.lock(basePath, {
+        retries: {
+            maxTimeout: 10000,
+            forever: true
+        }
+    });
     try{
-        console.log(tmpPath);
-        await pipeline(
-            request(tarURL),
-            tar.x({ strip: 1, cwd: tmpPath })
-        );
-        const profilePath = path.join(tmpPath, 'netlify-tex.profile');
-        await fs.writeFile(profilePath, profile(basePath, arch));
-        const env = {...process.env, TEXLIVE_INSTALL_PREFIX: basePath};
-        await spawn(path.join(tmpPath, installerName), [`--profile=${profilePath}`], { stdio: 'inherit', cwd: basePath, env: env});
+        if (await fs.exists(checkPath)){
+            return;
+        }
+        const { path: tmpPath, cleanup } = await tmp.dir({ unsafeCleanup: true });
+        try{
+            await pipeline(
+                request(tarURL),
+                tar.x({ strip: 1, cwd: tmpPath })
+            );
+            const profilePath = path.join(tmpPath, 'netlify-tex.profile');
+            await fs.writeFile(profilePath, profile(basePath, arch));
+            await spawn(
+                path.join(tmpPath, 'install-tl'),
+                [`--profile=${profilePath}`],
+                { stdio: 'inherit', cwd: basePath}
+            );
+        }
+        finally {
+            await cleanup();
+        }
     }
-    finally{
-        cleanup();
+    finally {
+        await release();
     }
 };
 
 const findTexlive = async () => {
-    
+    const basePath = process.env.NETLIFY_BUILD_BASE ? path.join(process.env.NETLIFY_BUILD_BASE, 'cache', '') : path.join(os.tmpdir(), 'netlify-tex');
+    if (searchForLocalTexlive && !process.env.NETLIFY_BUILD_BASE){
+        try{
+            const texPath = await fs.realpath(await which('tex'));
+            return path.dirname(texPath);
+        }
+        catch{}
+    }
+    await fs.ensureDir(basePath);
+    await installTexlive(basePath);
+    return path.join(basePath, 'latest', 'bin', arch);
 };
 
-const compile = async () => {
-
+const compile = async (texSource, output, cwd=process.cwd()) => {
+    const binPath = await findTexlive();
+    const { path: tmpPath, cleanup } = await tmp.dir({ unsafeCleanup: true });
+    try{
+        await fs.writeFile(path.join(tmpPath, 'root.tex'), texSource);
+        await spawn('python', [
+            path.join(binPath, '..', '..', 'texmf-dist', 'scripts', 'texliveonfly', 'texliveonfly.py'),
+            '-c',
+            'latexmk',
+            '-a',
+            `"-g -pdflua -interaction=nonstopmode -outdir=${tmpPath}"`,
+            path.join(tmpPath, 'root.tex')
+        ], {
+            stdio: 'inherit',
+            shell: true,
+            cwd: cwd,
+            env: {
+                ...process.env,
+                PATH: `${binPath}:${process.env.PATH}`
+            }
+        });
+        await fs.copyFile(path.join(tmpPath, 'root.pdf'), output);
+    }
+    finally{
+        await cleanup();
+    }
 };
 
 export default compile;
--- src/test.js
+++ src/test.js
@@ -1,5 +1,20 @@
-import { installTexlive } from './index'
+import compile from './index'
 
-installTexlive('/tmp/test-tex').then(() => {
-    console.log('Successfully installed Texlive');
+const texSource = `
+\\documentclass{article}
+\\usepackage{tikz}
+\\usepackage{amsmath}
+\\usepackage{cleveref}
+\\begin{document}
+    Hello there. This is some test. \\cref{eq:test}
+    \\begin{equation}\\label{eq:test}
+        x^2+1=\\int_0^\\infty y\\cdot dy
+    \\end{equation}
+\\end{document}`;
+
+// findTexlive().then(result => {
+//     console.log(result);
+// });
+compile(texSource, '/tmp/some.pdf').then(() => {
+    console.log('Compiled successfully!');
 });
